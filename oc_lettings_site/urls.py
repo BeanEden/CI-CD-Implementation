35a8b8a30940e9420646cb4afccf70e5426085a1
JC <bean3den@gmail.com>
2022-11-09 21:41:06 +0100

    correcting db migrate issue with heroku

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from oc_lettings_app.views import index
-
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include(('oc_lettings_app.urls', 'oc_lettings_app'), namespace='oc_lettings_app')),
     path('lettings/', include(('lettings_app.urls', 'lettings_app'), namespace='lettings_app')),
     path('profiles/', include(('profiles_app.urls', 'profiles_app'), namespace='profiles_app')),
     path('admin/', admin.site.urls),
--- tests/test_home.py
+++ tests/test_home.py
@@ -5,9 +5,9 @@ from django.test import Client
 
 
 @pytest.mark.django_db
-def test_profiles_app_index_view():
+def test_index_view():
     client = Client()
-    path = reverse('profiles_app:index')
+    path = reverse('oc_lettings_app:index')
     response = client.get(path)
     content = response.content.decode()
     expected_content = "<title>Holiday Homes</title>"
